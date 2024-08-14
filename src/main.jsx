70ae29680c0582f22a3761adf8ca7f2e3f90e504
zm-chan <zmc951230@gmail.com>
2024-08-15 02:54:16 +0800

    Make correction to main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <App />,
-  // <StrictMode>
-  // </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
